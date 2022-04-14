082080bdf989f54eb1855372516782bf579c82ac
Narayan Pote <n.pote7991@gmail.com>
2022-04-15 02:14:18 +0530

    Update ResumeNew.js

--- src/pages/ResumeNew.js
+++ src/pages/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "./pages/N_MERN_Resume.pdf";
+import pdf from "./N_MERN_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
