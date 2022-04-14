0aac2462dd5a08148d3b9b9126caec12204de384
Narayan Pote <n.pote7991@gmail.com>
2022-04-15 02:12:18 +0530

    Update ResumeNew.js

--- src/pages/ResumeNew.js
+++ src/pages/ResumeNew.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "./pages/N_MERN_Resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
